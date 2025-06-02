d6c0e84366615b8e9d94cf57de1192a6f965fb06
Simon Chanzu <chanzusimon6@gmail.com>
2025-06-02 17:38:04 +0300

    commits

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 
 import { MainLayout } from './layouts/MainLayout'
-import { Dashboard } from './pages/Dashboard'
-import { Orders } from './pages/Orders'
-import { Deliveries } from './pages/Deliveries'
-import { Payments } from './pages/Payments'
-import { Customers } from './pages/Customers'
-import { Reports } from './pages/Reports'
-import { Settings } from './pages/Settings'
-import { NewOrder } from './pages/NewOrder'
-import { NotFound } from './pages/NotFound'
+import  Dashboard  from './pages/Dashboard'
+import Orders  from './pages/Orders/Index'
+import Deliveries  from './pages/Deliveries'
+import Payments  from './pages/Payments'
+import  Customers  from './pages/Customers'
+import  Reports  from './pages/Reports'
+import  Settings  from './pages/Settings'
+import  NewOrder  from './pages/Orders/NewOrder'
+import  NotFound  from './pages/NotFound'
 
 function App() {
   return (
     <BrowserRouter>
+    <AnimatePresence mode="wait">
+
       <Routes>
         {/* Main Layout with sidebar and navbar */}
         <Route path="/" element={<MainLayout />}>
@@ -35,6 +37,8 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
+      </AnimatePresence>
+
     </BrowserRouter>
   )
 }
